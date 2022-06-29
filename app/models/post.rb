e4c53ac34787a56ba2e8f061d5737b17e9567c91
omar3o3 <otantawy53@gmail.com>
2022-06-29 17:23:21 -0400

    commit tree f97b8024094ce460aa6535b776c4885d167c92ce

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validate :contains_click_bait
+
+    def contains_click_bait
+        # unless title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+        unless title =~ /Won't Believe/i || /Secret/i || /Top \d/i || /Guess/i
+            errors.add(:title, 'Title must be clickbait')
+        end
+    end
+
+
 end
